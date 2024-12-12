9dd253fcfd90b61b6ef2bbeca70d7d41b14319ff
Sultan <sc23s2a@leeds.ac.uk>
2024-12-12 03:17:11 +0100

    cleaning up

--- build/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_watertool_FILE /Users/Sultan/Desktop/User Interfaces/Github Repository/comp2811-UI/build/watertool.app/Contents/MacOS/watertool)
-set(__QT_DEPLOY_TARGET_watertool_TYPE EXECUTABLE)
